091748e6fab5cf1d815e39f7349c0bb20b94f7e9
pi5-np <caio_mcoelho@hotmail.com>
2021-10-11 18:51:41 -0300

    Adiciona a autenticação do Token JWT

--- src/main/kotlin/br/com/newtonpaiva/pi5sendCloud/controller/SendCloudController.kt
+++ src/main/kotlin/br/com/newtonpaiva/pi5sendCloud/controller/SendCloudController.kt
@@ -3,35 +3,56 @@ package br.com.newtonpaiva.pi5sendCloud.controller
 import br.com.newtonpaiva.pi5sendCloud.dto.ObjectDTO
 import br.com.newtonpaiva.pi5sendCloud.entity.MessageEntity
 import br.com.newtonpaiva.pi5sendCloud.service.SendCloudService
+import br.com.newtonpaiva.pi5sendCloud.service.ValidateJwt
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/send")
 class SendCloudController(
-        private val sendCloudService: SendCloudService
+        private val sendCloudService: SendCloudService,
+        private val validateJwt: ValidateJwt
 ) {
 
     @PostMapping
-    fun endpointAll(@RequestBody messageEntity: MessageEntity){
-        val responseConnection: ObjectDTO? = sendCloudService.createConnection()
-        val accountId = responseConnection?.accountId
-        val apiUrl = responseConnection?.apiUrl
-        val authorizationToken = responseConnection?.authorizationToken
-        if(accountId != null && apiUrl != null && authorizationToken != null){
-            val responseBucket = sendCloudService.listBuckets(apiUrl, authorizationToken, accountId)
-            val responseFindBucket = sendCloudService.findBucketName(responseBucket, messageEntity.username!!)
-            if(responseFindBucket != ""){
-                println(("BUCKET ID: $responseFindBucket"))
-                sendCloudService.uploadFile(responseFindBucket!!, messageEntity.message!!, messageEntity.name!!)
+    fun endpointAll(@RequestBody messageEntity: MessageEntity, request: HttpServletRequest): ResponseEntity<Any>{
+
+        try {
+            val tokenResponse = validateJwt.getJwtFromRequest(request)
+            if(tokenResponse != ""){
+                validateJwt.validateToken(tokenResponse)
             } else {
-                val responseCreatedBucket = sendCloudService.createBucket(apiUrl, accountId, authorizationToken, messageEntity.username)
-                if(responseCreatedBucket != ""){
-                    println(("BUCKET ID: $responseCreatedBucket"))
-                    sendCloudService.uploadFile(responseCreatedBucket!!, messageEntity.message!!, messageEntity.name!!)
-                } else {
-                    println("ERROR!")
-                }
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Token is not found!")
             }
+            try {
+                val responseConnection: ObjectDTO? = sendCloudService.createConnection()
+                val accountId = responseConnection?.accountId
+                val apiUrl = responseConnection?.apiUrl
+                val authorizationToken = responseConnection?.authorizationToken
+                if(accountId != null && apiUrl != null && authorizationToken != null){
+                    val responseBucket = sendCloudService.listBuckets(apiUrl, authorizationToken, accountId)
+                    val responseFindBucket = sendCloudService.findBucketName(responseBucket, messageEntity.username!!)
+                    if(responseFindBucket != ""){
+                        println(("BUCKET ID: $responseFindBucket"))
+                        sendCloudService.uploadFile(responseFindBucket!!, messageEntity.message!!, messageEntity.name!!)
+                    } else {
+                        val responseCreatedBucket = sendCloudService.createBucket(apiUrl, accountId, authorizationToken, messageEntity.username)
+                        if(responseCreatedBucket != ""){
+                            println(("BUCKET ID: $responseCreatedBucket"))
+                            sendCloudService.uploadFile(responseCreatedBucket!!, messageEntity.message!!, messageEntity.name!!)
+                        } else {
+                            println("ERROR!")
+                        }
+                    }
+                }
+                return ResponseEntity.ok().body("")
+            } catch (e: Exception){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.message)
+            }
+        } catch (e: Exception){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.message)
         }
     }
 }
